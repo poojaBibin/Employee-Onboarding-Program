578be14984b6dad14df17c0bf44173c841c0d02a
poojaBibin <145107035+poojaBibin@users.noreply.github.com>
2023-09-27 02:21:59 +0530

    Add files via upload

---
+++ precache-manifest.f81b16ee0d3d875c2715e5d85e997693.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8cd594613aed7d2fa8d52a971d01344",
+    "url": "@yield('content-viewer-url')/index.html"
+  },
+  {
+    "revision": "3c1c92abc9e6eb3d4d45",
+    "url": "@yield('content-viewer-url')/static/css/2.37be29a2.chunk.css"
+  },
+  {
+    "revision": "f7031748d22b1218637f",
+    "url": "@yield('content-viewer-url')/static/css/main.bf30fa06.chunk.css"
+  },
+  {
+    "revision": "3c1c92abc9e6eb3d4d45",
+    "url": "@yield('content-viewer-url')/static/js/2.8be722d5.chunk.js"
+  },
+  {
+    "revision": "73a2cb6665b648320b93dbc14dbfa5c3",
+    "url": "@yield('content-viewer-url')/static/js/2.8be722d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7031748d22b1218637f",
+    "url": "@yield('content-viewer-url')/static/js/main.2973960d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "@yield('content-viewer-url')/static/js/main.2973960d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33a12fe361989d2d462",
+    "url": "@yield('content-viewer-url')/static/js/runtime-main.eac69593.js"
+  },
+  {
+    "revision": "a754c5ed00d8f6cca2a96794a2beaddc",
+    "url": "@yield('content-viewer-url')/static/media/trophy.a754c5ed.svg"
+  }
+]);
